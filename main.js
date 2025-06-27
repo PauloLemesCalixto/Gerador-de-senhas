2c827af0ace21dfe1d6176774d947c50df849adf
PauloLemesCalixto <calixto.paulo@escola.pr.gov.br>
2025-06-27 18:05:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeoroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        //tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        //tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+
+campoSenha.value = letrasMaiuscula;
